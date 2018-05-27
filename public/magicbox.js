a5b1eea8d8155b1c36b8ee3953dc9a10305e52bd
yanhackcode15 <iamyanhu@gmail.com>
2018-05-26 21:28:34 -0700

    Overhead video setup

--- public/magicbox.js
+++ public/magicbox.js
@@ -1,44 +1,30 @@
 const PlayStates = Object.freeze({
     Screensaver: 'Screensaver',
-    Magicbox: 'Magicbox',
 });
 
 let state;
-document.addEventListener('readystatechange', initMagicbox);
+document.addEventListener('readystatechange', init);
 
-function initMagicbox() {
+function init() {
+    console.log('init')
     if (!state) {
         const screensaver = document.getElementById('screensaver');
-        const magicbox = document.getElementById('magicbox');
         state = {
             screensaver,
-            magicbox,
             playstate: PlayStates.Screensaver,
         };
-        document.body.addEventListener('mousedown', playMagicbox);
-        document.body.addEventListener('keydown', playMagicbox);
-        document.body.addEventListener('touchstart', playMagicbox);
-        magicbox.addEventListener('ended', playScreensaver);
+        document.body.addEventListener('mousedown', playScreensaver);
+        document.body.addEventListener('keydown', playScreensaver);
+        document.body.addEventListener('touchstart', playScreensaver);
+        screensaver.addEventListener('ended', playScreensaver);
         playScreensaver();
     }
 }
 
-function playMagicbox() {
-    if (state.playstate !== PlayStates.Magicbox) {
-        state.playstate = PlayStates.Magicbox;
-        state.magicbox.style.display = 'block';
-        state.magicbox.currentTime = 0;
-        state.magicbox.play();
-        state.screensaver.style.display = 'none';
-        state.screensaver.pause();
-    }
-}
-
 function playScreensaver() {
+    console.log('play')
     state.playstate = PlayStates.Screensaver;
     state.screensaver.style.display = 'block';
     state.screensaver.currentTime = 0;
     state.screensaver.play();
-    state.magicbox.style.display = 'none';
-    state.magicbox.pause();
 }
